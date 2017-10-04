b0f8c00f30c5b3ad96f750efe2f9f2fef18e5c2a
Allen Kim <allenkim67@gmail.com>
2017-10-04 15:07:15 +0700

    List out methods for start_game

--- Blackjack.py
+++ Blackjack.py
@@ -5,4 +5,32 @@ class Blackjack:
 
 
     def start_game(self):
+        print('Welcome to blackjack!\n')
+
+        while self.keep_playing():
+            self.take_bets()
+            self.deal_hands()
+            self.play_out_hands()
+            self.make_payouts()
+
+        print('Thanks for playing!')
+
+
+    def keep_playing(self):
+        pass
+
+
+    def take_bets(self):
+        pass
+
+
+    def deal_hands(self):
+        pass
+
+
+    def play_out_hands(self):
+        pass
+
+
+    def make_payouts(self):
         pass
